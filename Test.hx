e85b3324e1f870dfaabc9b1d2230ec07d0f9df97
dogura <daiki.ogura@soliton.co.jp>
2014-12-15 16:23:11 +0900

    add test for orElse

--- Test.hx
+++ Test.hx
@@ -1,11 +1,25 @@
 
 import ds.Result;
-import ds.mixins.OptionAsMonad;
 import ds.mixins.ResultAsMonad;
+
+using ds.mixins.OptionAsMonad;
+
+class TestOption extends haxe.unit.TestCase {
+  public function test_orElse() {
+    var x: String = null;
+    assertEquals(x.toOption().orElse(Some('bb')).getOrElse(''), 'bb');
+
+    var xa: String = 'aa';
+    assertEquals(xa.toOption().orElse(Some('bb')).getOrElse(''), 'aa');
+  }
+}
+
 
 class Test {
 
   public static function main() {
-    trace('aa');
+    var runner = new haxe.unit.TestRunner();
+    runner.add(new TestOption());
+    runner.run();
   }
 }
